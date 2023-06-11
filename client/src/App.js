2f427b7fc8cc6ef363f2cb37393e91459e31b7b3
Saoa35 <saoamah@gmail.com>
2023-06-11 15:17:02 +0400

    installed & connected tailwind

---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-2xl text-red-600">TEXT</div>;
+}
+
+export default App;
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -68,5 +68,25 @@ export const login = async (req, res) => {
 
 export const getMe = async (req, res) => {
   try {
-  } catch (error) {}
+    const user = await User.findById(req.userId);
+
+    if (!user) {
+      return res.json({ message: `User does not exist` });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "30d" }
+    );
+
+    res.json({
+      user,
+      token,
+    });
+  } catch (error) {
+    res.json({ message: "No access" });
+  }
 };
